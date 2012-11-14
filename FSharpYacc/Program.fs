32ba7abe58049414a6b21e2c646d18a5c6e6a49b
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-11-14 10:42:50 -0500

    - Created a (basically empty) project for fsharpyacc so we can start experimenting with it.

---
+++ FSharpYacc/Program.fs
@@ -0,0 +1,15 @@
+﻿(*
+Copyright (c) 2012, Jack Pappas
+All rights reserved.
+
+This code is provided under the terms of the 2-clause ("Simplified") BSD license.
+See LICENSE.TXT for licensing details.
+*)
+
+module FSharpYacc.Program
+
+
+
+printfn "Press any key to exit..."
+System.Console.ReadKey ()
+|> ignore
