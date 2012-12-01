60258cb6da0f1ab6646dd255636806f531015267
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-12-01 11:48:37 -0500

    - Created placeholder modules for the lexer and parser which we'll eventually add.

---
+++ FSharpLex/Parser.fs
@@ -0,0 +1,10 @@
+﻿(*
+Copyright (c) 2012, Jack Pappas
+All rights reserved.
+
+This code is provided under the terms of the 2-clause ("Simplified") BSD license.
+See LICENSE.TXT for licensing details.
+*)
+
+module Parser
+
