a2bab1427400367742b95dbe3eb9b615559d2158
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-12-03 14:25:51 -0500

    - Created an empty new module, GraphGen, to hold backends which generate DGML or dot files for viewing the compiled DFAs.

---
+++ FSharpLex/GraphGen.fs
@@ -0,0 +1,21 @@
+﻿(*
+Copyright (c) 2012, Jack Pappas
+All rights reserved.
+
+This code is provided under the terms of the 2-clause ("Simplified") BSD license.
+See LICENSE.TXT for licensing details.
+*)
+
+//
+module FSharpLex.GraphGen
+
+open System.ComponentModel.Composition
+open System.IO
+//open System.Xml
+open SpecializedCollections
+open Ast
+open Compile
+
+// TODO : Implement back-ends which generate graph output, e.g., for debugging and presentation.
+// DGML (for Visual Studio)
+// dot (for graphviz)
