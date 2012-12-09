512b426b72431a2aa89623b3c75a978e406d76e9
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-12-09 09:43:51 -0500

    - Created empty modules as placeholders for the lexer and parser we'll eventually add.

---
+++ FSharpYacc/Lexer.fs
@@ -0,0 +1,11 @@
+﻿(*
+Copyright (c) 2012, Jack Pappas
+All rights reserved.
+
+This code is provided under the terms of the 2-clause ("Simplified") BSD license.
+See LICENSE.TXT for licensing details.
+*)
+
+//
+module FSharpYacc.Lexer
+
