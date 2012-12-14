0b58608516b3ab9c66a3b50d3f39631c79e44d8f
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-12-14 16:01:39 -0500

    - Created Options.fs to hold types for representing compiler options; these reside directly in the FSharpYacc namespace.

---
+++ FSharpYacc/Options.fs
@@ -0,0 +1,17 @@
+﻿(*
+Copyright (c) 2012, Jack Pappas
+All rights reserved.
+
+This code is provided under the terms of the 2-clause ("Simplified") BSD license.
+See LICENSE.TXT for licensing details.
+*)
+
+namespace FSharpYacc
+
+//
+type CompilationOptions = {
+    //
+    InputFile : string;
+
+}
+
