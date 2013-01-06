791f5f2d9a5fbc95df1f7f4b10dcd628812870f4
Jack Pappas <jackp@server.fake>
2013-01-06 14:21:13 -0500

    - Modified the test code to use our new generic implementation of Assert.AreEqual to avoid typing-related issues when running the unit tests. - As of this commit, the unit tests compile successfully again and can be run with NUnit 2.6.2; the table-generation tests are currently failing, however, due to some changes in the way we generate the parser tables (the numbering of the parser states is different).

--- Graham.Tests/Predictive.fs
+++ Graham.Tests/Predictive.fs
@@ -12,6 +12,7 @@ open Grammars
 
 open NUnit.Framework
 open FsUnit
+open TestHelpers
 
 open Graham.Grammar
 open Graham.Analysis
@@ -25,68 +26,76 @@ let ``Analysis of Grammar 3.26`` () =
     (* Verify the nullable map. *)
     // The nullable map should have exactly the same number
     // of entries as the grammar has non-terminals.
-    Set.count grammar.Nonterminals
-    |> should equal predictiveSets.Nullable.Count
+    Assert.AreEqual (
+        Set.count grammar.Nonterminals,
+        predictiveSets.Nullable.Count)
 
     // All non-terminals should have an entry.
     grammar.Nonterminals
     |> Set.forall (fun nonterm ->
         Map.containsKey nonterm predictiveSets.Nullable)
-    |> should be True
+    |> Assert.IsTrue
 
     // For this grammar, none of the non-terminals are nullable.
     predictiveSets.Nullable
     |> Map.exists (fun _ v -> v)
-    |> should be False
+    |> Assert.IsFalse
 
     (* Verify the FIRST sets of the nonterminals. *)
     // The map of FIRST sets should have exactly the same number
     // of entries as the grammar has non-terminals.
-    Set.count grammar.Nonterminals
-    |> should equal predictiveSets.First.Count
+    Assert.AreEqual (
+        Set.count grammar.Nonterminals,
+        predictiveSets.First.Count)
 
     // All non-terminals should have an entry.
     grammar.Nonterminals
     |> Set.forall (fun nonterm ->
         Map.containsKey nonterm predictiveSets.First)
-    |> should be True
+    |> Assert.IsTrue
 
     // Verify the entries are correct.
     let firstSet =
         Set.ofArray [|
-            Terminal "*";
-            Terminal "x"; |]
+            AugmentedTerminal.Terminal "*";
+            AugmentedTerminal.Terminal "x"; |]
     grammar.Nonterminals
     |> Set.iter (fun nonterm ->
-        Map.find nonterm predictiveSets.First
-        |> should equal firstSet)
+        Assert.AreEqual (
+            Map.find nonterm predictiveSets.First,
+            firstSet))
         
     (* Verify the FOLLOW sets of the nonterminals. *)
     // The map of FOLLOW sets should have exactly the same number
     // of entries as the grammar has non-terminals.
-    Set.count grammar.Nonterminals
-    |> should equal predictiveSets.Follow.Count
+    Assert.AreEqual (
+        Set.count grammar.Nonterminals,
+        predictiveSets.Follow.Count)
 
     // All non-terminals should have an entry.
     grammar.Nonterminals
     |> Set.forall (fun nonterm ->
         Map.containsKey nonterm predictiveSets.Follow)
-    |> should be True
+    |> Assert.IsTrue
 
     // Verify the entries are correct.
-    Map.find (AugmentedNonterminal.Nonterminal 'E') predictiveSets.Follow
-    |> should equal <| Set.ofArray [|
-        AugmentedTerminal.Terminal "="; EndOfFile; |]
+    Assert.AreEqual (
+        Map.find (AugmentedNonterminal.Nonterminal 'E') predictiveSets.Follow,
+        Set.ofArray [|
+            AugmentedTerminal.Terminal "="; EndOfFile; |])
 
-    Map.find (AugmentedNonterminal.Nonterminal 'S') predictiveSets.Follow
-    |> should equal <| Set.ofArray (
-        [| EndOfFile; |] : AugmentedTerminal<string>[])
+    Assert.AreEqual (
+        Map.find (AugmentedNonterminal.Nonterminal 'S') predictiveSets.Follow,
+        Set.ofArray (
+            [| EndOfFile; |] : AugmentedTerminal<string>[]))
 
-    Map.find (AugmentedNonterminal.Nonterminal 'V') predictiveSets.Follow
-    |> should equal <| Set.ofArray [|
-        AugmentedTerminal.Terminal "="; EndOfFile; |]
+    Assert.AreEqual (
+        Map.find (AugmentedNonterminal.Nonterminal 'V') predictiveSets.Follow,
+        Set.ofArray [|
+            AugmentedTerminal.Terminal "="; EndOfFile; |])
 
-    Map.find Start predictiveSets.Follow
-    |> should equal <| Set.ofArray (
-        [| EndOfFile; |] : AugmentedTerminal<string>[])
+    Assert.AreEqual (
+        Map.find Start predictiveSets.Follow,
+        Set.ofArray (
+            [| EndOfFile; |] : AugmentedTerminal<string>[]))
 
