e096e2aa040f28634a7054ab3152014ffb31312d
Jack Pappas <jackp@server.fake>
2013-01-06 14:11:06 -0500

    - Created an internal TestHelpers module which contains a type, Assert, which implements some generic wrappers around NUnit.Framework.Assert methods; these are used instead of the 'obj'-based Assert methods to avoid type-related exceptions when running the unit tests (without the generic methods, the compiler will happily compile invalid code by silently upcasting to 'obj').

---
+++ Graham.Tests/TestHelpers.fs
@@ -0,0 +1,23 @@
+﻿(*
+Copyright (c) 2012, Jack Pappas
+All rights reserved.
+
+This code is provided under the terms of the 2-clause ("Simplified") BSD license.
+See LICENSE.TXT for licensing details.
+*)
+
+/// Internal helper classes for implementing unit tests.
+module internal Graham.Tests.TestHelpers
+
+
+/// Helper class that provides generic "overloads" of some Assert methods.
+[<AbstractClass; Sealed>]
+type Assert =
+    /// <summary>Verifies that two values are equal.
+    /// If they are not, then an NUnit.Framework.AssertException is thrown.</summary>
+    /// <param name="expected">The expected value.</param>
+    /// <param name="actual">The actual value.</param>
+    static member AreEqual<'T when 'T : equality> (expected : 'T, actual : 'T) : unit =
+        NUnit.Framework.Assert.AreEqual (expected, actual)
+
+
