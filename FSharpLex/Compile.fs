96ad786d955e94874c37f40c210b60b248dedce9
Jack Pappas <jackp@server.fake>
2013-01-21 09:25:24 -0500

    - Use a Map for now instead of the intermediate Dictionary, just to keep things simple.

--- FSharpLex/Compile.fs
+++ FSharpLex/Compile.fs
@@ -236,31 +236,40 @@ module private EncodingCharSet =
 //
 [<RequireQualifiedAccess>]
 module private Unicode =
+    open System
+    open System.Globalization
+
     /// Maps each UnicodeCategory to the set of characters in the category.
     let categoryCharSet =
+        // Initialize the map so it contains an empty set for each Unicode category.
+        // This serves two purposes:
+        //  (a) To ensure the completed map contains an entry for each Unicode category,
+        //      so we can just use Map.find instead of Map.tryFind.
+        //  (b) It avoids a branch in the loop below.
+        let mutable categoryChars =
+            let unicodeCategories =
+                Enum.GetValues typeof<UnicodeCategory> :?> UnicodeCategory[]
+
+            (Map.empty, unicodeCategories)
+            ||> Array.fold (fun categoryChars category ->
+                Map.add category CharSet.empty categoryChars)
+
         // OPTIMIZE : If this takes "too long" to compute on-the-fly, we could pre-compute
         // the category sets and implement code which recreates the CharSets from the intervals
         // in the CharSets (not the individual values, which would be much slower).
-        let table = System.Collections.Generic.Dictionary<_,_> (30)
+        // OPTIMIZE : If necessary, we could also use an array here instead of a Dictionary,
+        // since the number of elements is small and static.
         for i = 0 to 65535 do
             /// The Unicode category of this character.
-            let category = System.Char.GetUnicodeCategory (char i)
+            let category = Char.GetUnicodeCategory (char i)
 
             // Add this character to the set for this category.
-            table.[category] <-
-                match table.TryGetValue category with
-                | true, charSet ->
-                    CharSet.add (char i) charSet
-                | false, _ ->
-                    CharSet.singleton (char i)
+            categoryChars <-
+                let charSet = Map.find category categoryChars
+                Map.add category (CharSet.add (char i) charSet) categoryChars
 
-        // TODO : Assert that the table contains an entry for every UnicodeCategory value.
-        // Otherwise, exceptions will be thrown at run-time if we try to retrive non-existent entries.
-
-        // Convert the dictionary to a Map
-        (Map.empty, table)
-        ||> Seq.fold (fun categoryMap kvp ->
-            Map.add kvp.Key kvp.Value categoryMap)
+        // Return the constructed map.
+        categoryChars
 
 //
 let private rulePatternsToDfa (rulePatterns : RegularVector) (patternIndices : RuleClauseIndex[]) (options : CompilationOptions) : LexerRuleDfa =
