38855654bea95ada3e5763c2ed1d81541455c3f1
Jack Pappas <jackp@server.fake>
2013-01-31 08:19:36 -0500

    - Don't compile rules in parallel, but allow derivatives to be computed in parallel.

--- FSharpLex/Compile.fs
+++ FSharpLex/Compile.fs
@@ -1103,11 +1103,7 @@ let lexerSpec (spec : Specification) options =
         let compiledRules, compilationErrors =
             let compiledRulesOrErrors =
                 rules
-                #if PARALLEL_FX
-                |> Array.Parallel.map (fun rule ->
-                #else
                 |> Array.map (fun rule ->
-                #endif
                     compileRule rule options (macroEnv, Set.empty))
 
             let compiledRules = ResizeArray<_> (Array.length rules)
