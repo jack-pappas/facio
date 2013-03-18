3edc4ea63e7a224c1d6d63b00c283b33a4b13b25
Jack Pappas <jackp@server.fake>
2013-03-18 13:31:25 -0400

    Added some grammars from "Parsing Techniques, A Practical Guide" so they can be used as test cases for grammar optimizations (when we implement them).

--- Graham.Tests/Grammars.Grune.fs
+++ Graham.Tests/Grammars.Grune.fs
@@ -22,6 +22,93 @@ module Graham.Tests.Grammars.Grune
 open Graham.Grammar
 
 
-//
+/// Figure 4.4: A grammar describing numbers in scientific notation.
 let ``Figure 4.4`` =
-    ()
+    let ``Figure 4.4`` =
+        // NOTE : This is the start symbol.
+        let Number =
+            [|  [| Nonterminal "Integer" |];
+                [| Nonterminal "Real" |];   |]
+
+        let Integer =
+            [|  [| Nonterminal "Digit" |];
+                [| Nonterminal "Integer"; Nonterminal "Digit" |];   |]
+
+        let Real =
+            [|  [| Nonterminal "Integer"; Nonterminal "Fraction"; Nonterminal "Scale" |];   |]
+
+        let Fraction =
+            [|  [| Terminal '.'; Nonterminal "Integer" |];  |]
+
+        let Scale =
+            [|  [| Terminal 'e'; Nonterminal "Sign"; Nonterminal "Integer" |];
+                [| Nonterminal "Empty" |];  |]
+
+        let Digit =
+            [|  [| Terminal '0' |];
+                [| Terminal '1' |];
+                [| Terminal '2' |];
+                [| Terminal '3' |];
+                [| Terminal '4' |];
+                [| Terminal '5' |];
+                [| Terminal '6' |];
+                [| Terminal '7' |];
+                [| Terminal '8' |];
+                [| Terminal '9' |]; |]
+
+        let Empty =
+            [|  [| |] |]
+
+        let Sign =
+            [|  [| Terminal '+' |];
+                [| Terminal '-' |]; |]
+
+        let productions =
+            Map.empty
+            |> Map.add "Number" Number
+            |> Map.add "Integer" Integer
+            |> Map.add "Real" Real
+            |> Map.add "Fraction" Fraction
+            |> Map.add "Scale" Scale
+            |> Map.add "Digit" Digit
+            |> Map.add "Empty" Empty
+            |> Map.add "Sign" Sign
+
+        let nonterminals, terminals =
+            Grammar.SymbolSets productions
+
+        {   Terminals = terminals;
+            Nonterminals = nonterminals;
+            Productions = productions; }
+
+    // Augment the grammar.
+    Grammar.Augment (``Figure 4.4``, "Number")
+
+/// Figure 4.8: An example grammar to test epsilon-rule elimination schemes.
+let ``Figure 4.8`` =
+    let S =
+        [|  [| Nonterminal 'L'; Terminal 'a'; Nonterminal 'M' |];   |]
+
+    let L =
+        [|  [| Nonterminal 'L'; Nonterminal 'M' |];
+            [| |];  |]
+
+    let M =
+        [|  [| Nonterminal 'M'; Nonterminal 'M' |];
+            [| |];  |]
+
+    let productions =
+        Map.empty
+        |> Map.add 'S' S
+        |> Map.add 'L' L
+        |> Map.add 'M' M
+
+    let nonterminals, terminals =
+        Grammar.SymbolSets productions
+
+    {   Terminals = terminals;
+        Nonterminals = nonterminals;
+        Productions = productions; }
+
+
+
