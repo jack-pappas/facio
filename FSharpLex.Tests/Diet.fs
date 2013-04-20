a6482417b6aa8ab900fd6a6c3aeb27beb507694e
Jack Pappas <jackp@server.fake>
2013-04-20 18:47:01 -0400

    List test cases that need to be implemented.

--- FSharpLex.Tests/Diet.fs
+++ FSharpLex.Tests/Diet.fs
@@ -17,11 +17,54 @@ limitations under the License.
 *)
 
 /// Tests for the balanced DIET (Discrete Interval Encoding Tree).
-module FSharpLex.SpecializedCollections.Tests
+module Tests.FSharpLex.SpecializedCollections
 
 open FSharpLex.SpecializedCollections
 open NUnit.Framework
 open FsUnit
+open FsCheck
+
+(* TODO :   Modify these tests to use CharSet instead of CharDiet.
+            Change the CharDiet type abbreviation and module back to 'private'.
+            Remove any tests for CharDiet which don't have a corresponding method in CharSet. *)
+
+[<TestCase>]
+let dummy () : unit =
+    Assert.Inconclusive "Test not yet implemented."
 
 
-(* TODO : Implement tests for the DIET implementation *)
+// Tests for CharDiet:
+// find_del_left
+// find_del_right
+// isEmpty
+// contains
+// maxElement
+// minElement
+// bounds
+// comparison
+// equal
+// count
+// intervalCount
+// singleton
+// ofRange
+// add
+// addRange
+// remove
+// union
+// intersect
+// difference
+// fold
+// foldBack
+// iter
+// forall
+// exists
+// toSeq
+// toList
+// toArray
+// toSet
+
+// Tests for CharSet
+// ofSeq
+// ofList
+// ofArray
+// ofSet
