90090970e95859d423d969fb69d91e10ee0b03c4
Daniel Fabian <daniel.fabian@integral-it.ch>
2014-04-07 15:16:37 +0200

    adjust .targets.

--- FSharp.Tools.Tasks/FsharpLex.Build.fs
+++ FSharp.Tools.Tasks/FsharpLex.Build.fs
@@ -9,10 +9,11 @@ open Microsoft.Build.Utilities
 (**************************************
 MSBuild task for fsharplex
 
-options:
-        -o <string>: Name the output file.
+fsharplex <filename>
+        -o <string>: The path where the generated lexer source code will be written.
         --codepage <int>: Assume input lexer specification file is encoded with the given codepage.
-        --unicode: Produce a lexer for use with 16-bit unicode characters.
+        --lexlib <string>: Specify the namespace for the implementation of the lexer table interpreter. The default is 'Microsoft.FSharp.Text.Lexing'.
+        --unicode: Produce a lexer which supports 16-bit Unicode characters.
         --help: display this list of options
         -help: display this list of options
 **************************************)
@@ -20,22 +21,22 @@ options:
 type FsLex() = 
     inherit ToolTask()
 
-    // [<Required>]
-    member val InputFile = "" with get, set
+    [<Required>]
+    member val InputFile = null : string with get, set
     
     [<Output>]
-    member val OutputFile = "" with get, set
+    member val OutputFile = null : string with get, set
     
     // --codepage <int>: Assume input lexer specification file is encoded with the given codepage.
-    member val CodePage = "" with get, set
+    member val CodePage = null : string with get, set
     
     // --unicode: Produce a lexer for use with 16-bit unicode characters.
     member val Unicode = false with get, set
 
-    member val OtherFlags = "" with get, set
+    member val LexLib = null : string with get, set
 
     // ToolTask methods
-    override this.ToolName = "fslex.exe"
+    override this.ToolName = "fsharplex.exe"
     
     override this.GenerateFullPathToTool() = System.IO.Path.Combine(this.ToolPath, this.ToolExe)
         
@@ -52,15 +53,12 @@ type FsLex() =
         // OutputFile
         builder.AppendSwitchIfNotNull("-o ", this.OutputFile)
 
-        // OtherFlags - must be second-to-last
-        builder.AppendSwitchUnquotedIfNotNull("", this.OtherFlags)
+        builder.AppendSwitchUnquotedIfNotNull("--lexlib ", this.LexLib)
 
         builder.AppendSwitchIfNotNull(" ", this.InputFile)
         
         let args = builder.ToString()
 
-        // when doing simple unit tests using API, no BuildEnginer/Logger is attached
-        if this.BuildEngine <> null then
-            let eventArgs = { new CustomBuildEventArgs(message=args,helpKeyword="",senderName="") with member x.Equals(y) = false }
-            this.BuildEngine.LogCustomEvent(eventArgs)
+        this.BuildEngine.LogCustomEvent { new CustomBuildEventArgs(message=args,helpKeyword="",senderName="") with member x.Equals(y) = false }
+        
         args
--- FSharp.Tools.Tasks/FsharpYacc.Build.fs
+++ FSharp.Tools.Tasks/FsharpYacc.Build.fs
@@ -8,47 +8,72 @@ open Microsoft.Build.Utilities
 
 (**************************************
 MSBuild task for fsharpyacc
+
+fsharpyacc <filename>
+        -o <string>: The path where the output file will be written.
+        -v: Produce a listing file.
+        --module <string>: The name to use for the F# module containing the generated parser. The default is 'Parser'.
+        --internal: Generate an internal module
+        --open <string>: Add the given module to the list of those to open in both the generated signature and implementation.
+        --lexlib <string>: Specify the namespace for the implementation of the lexer table interpreter. The default is 'Microsoft.FSharp.Text.Lexing'.
+        --parslib <string>: Specify the namespace for the implementation of the parser table interpreter. The default is 'Microsoft.FSharp.Text.Parsing'.
+        --codepage <int>: Assume input lexer specification file is encoded with the given codepage.
+        --help: display this list of options
+        -help: display this list of options
 **************************************)
 
 type FsharpYacc() = 
     inherit ToolTask()
-    // [<Required>]
-    member val InputFile = "" with get, set
     
-    [<Microsoft.Build.Framework.Output>]
-    member val OutputFile = "" with get, set
+    [<Required>]
+    member val InputFile = null : string with get, set
     
-    member val OtherFlags = "" with get, set
-
+    [<Output>]
+    member val OutputFile = null : string with get, set
+    
     // --codepage <int>: Assume input lexer specification file is encoded with the given codepage.
-    member val CodePage = "" with get, set
+    member val CodePage = null : string with get, set
 
-    // --ml-compatibility: Support the use of the global state from the 'Parsing' module in MLLib.
-    member val MLCompatibility = false with get, set
-        
+    member val LexLib = null : string with get, set
+
+    member val ParsLib = null : string with get, set
+
     // --open
-    member val Open = "" with get, set
+    member val Open = null : string with get, set
 
-   // --module
-    member val Module = "" with get, set
+    // --module
+    member val Module = null : string with get, set
+
+    member val Verbose = false with get, set
+
+    member val Internal = false with get, set
 
     // ToolTask methods
-    override this.ToolName = "fsyacc.exe"
+    override this.ToolName = "fsharpyacc.exe"
     
     override this.GenerateFullPathToTool() = System.IO.Path.Combine(this.ToolPath, this.ToolExe)
         
     override this.GenerateCommandLineCommands() =
     
         let builder = new CommandLineBuilder()
+
+        // Verbose
+        if this.Verbose then builder.AppendSwitch("-v")
+        
+        // Internal
+        if this.Internal then builder.AppendSwitch("--internal")
         
         // CodePage
         builder.AppendSwitchIfNotNull("--codepage ", this.CodePage)
         
-        // ML Compatibility
-        if this.MLCompatibility then builder.AppendSwitch("--ml-compatibility")
-
         // Open
         builder.AppendSwitchIfNotNull("--open ", this.Open)
+
+        // LexLib
+        builder.AppendSwitchIfNotNull("--lexlib ", this.LexLib)
+
+        // LexLib
+        builder.AppendSwitchIfNotNull("--parslib ", this.ParsLib)
 
         // Module
         builder.AppendSwitchIfNotNull("--module ", this.Module)
@@ -56,8 +81,6 @@ type FsharpYacc() =
         // OutputFile
         builder.AppendSwitchIfNotNull("-o ", this.OutputFile)
 
-        // OtherFlags - must be second-to-last
-        builder.AppendSwitchUnquotedIfNotNull("", this.OtherFlags)
 
         builder.AppendSwitchIfNotNull(" ", this.InputFile)
         
