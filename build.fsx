4b4bf707e8c702542266f591168daa965cf6ea05
Daniel Fabian <daniel.fabian@integral-it.ch>
2014-04-07 19:49:55 +0200

    Add first draft of nuget package.

--- build.fsx
+++ build.fsx
@@ -6,15 +6,50 @@ open Fake
 let docDir = "./doc"
 let resultsDir = "./results"
 let docOutputDir = resultsDir @@ "Documentation"
+let nugetTemp = "./nugetTemp/"
 
 Target "Clean" (fun _ -> 
-    CleanDir resultsDir
+    CleanDirs [resultsDir; nugetTemp]
 )
 
 Target "Build" (fun _ ->
     !! "/**/*.sln"
-    |> MSBuildRelease resultsDir "Build"
-    |> Log "Build-Output: "
+    |> MSBuild resultsDir "Build" ["Configuration", "Testing"]
+    |> Log "Build-Output (testing): "
+
+    !! "/**/*.sln"
+    |> MSBuildRelease "" "Build"
+    |> Log "Build-Output (nuget): "
+)
+
+Target "FSharp.Tools Nuget" (fun _ ->
+    
+    let grahamInfo = !! @"C:\Users\Daniel\git\incube\credit-suisse\fsharp-tools/graham.nuspec" |> Seq.exactlyOne
+    let grahamInfo = (ReadFileAsString >> getNuspecProperties) grahamInfo
+    
+    let fsharpToolsNuget = !! "/**/Fsharp.Tools.nuspec" |> Seq.exactlyOne
+    
+    let nugetVersion = grahamInfo.Version
+    let workingDir = nugetTemp @@ (filenameWithouExt fsharpToolsNuget)
+    
+    ensureDirectory (workingDir @@ "lib" @@ "net40")
+    ensureDirectory (workingDir @@ "build")
+    
+    !! "/**/FSharp.Tools.Tasks/bin/Release/*.dll"
+    ++ "/**/FSharp.Tools.Tasks/bin/Release/*.exe"
+    ++ "/**/FSharp.Tools.Tasks/bin/Release/*.config"
+    |> CopyFiles (workingDir @@ "lib" @@ "net40")
+
+    !! "/**/bin/Release/FSharp.Tools.targets"
+    |> CopyFiles (workingDir @@ "build")
+    
+
+    NuGet (fun p -> 
+        { p with
+            WorkingDir = workingDir
+            OutputPath = resultsDir
+            Version = grahamInfo.Version })
+        fsharpToolsNuget
 )
 
 Target "Tests" DoNothing // TODO: implement something meaningful
@@ -24,6 +59,7 @@ Target "Default" DoNothing
 "Clean"
     ==> "Build"
     =?> ("Tests", not <| hasBuildParam "NoTests")
+    ==> "FSharp.Tools Nuget"
     ==> "Default"
 
 RunTargetOrDefault "Default"
